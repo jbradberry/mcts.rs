53095dbd67c40c13209b915376b9aebf02e101bb
Jeff Bradberry <jeff.bradberry@gmail.com>
2018-07-06 14:18:57 -0400

    Fix the bitmask to avoid overflow when considering the left and right edges

--- src/main.rs
+++ src/main.rs
@@ -131,16 +131,16 @@ impl BoardState<ChongAction, ChongPlayer> for ChongState {
 
                 // println!("{}, {}", pawn, value);
                 if (pawn << 8) == value || (pawn >> 8) == value { true }
-                else if ((pawn << 1) & 0x7f7f7f7f7f7f7f7f) == value { true }
-                else if ((pawn >> 1) & 0xfefefefefefefefe) == value { true }
+                else if ((pawn << 1) & 0xfefefefefefefefe) == value { true }
+                else if ((pawn >> 1) & 0x7f7f7f7f7f7f7f7f) == value { true }
                 else if (pawn << 16) == value && ((pawn << 8) & stones) != 0 { true }
                 else if (pawn >> 16) == value && ((pawn >> 8) & stones) != 0 { true }
-                else if ((pawn << 2) & 0x7f7f7f7f7f7f7f7f) == value && ((pawn << 1) & stones) != 0 { true }
-                else if ((pawn >> 2) & 0xfefefefefefefefe) == value && ((pawn >> 1) & stones) != 0 { true }
-                else if ((pawn << 14) & 0x7f7f7f7f7f7f7f7f) == value && ((pawn << 7) & stones) != 0 { true }
-                else if ((pawn >> 14) & 0xfefefefefefefefe) == value && ((pawn >> 7) & stones) != 0 { true }
-                else if ((pawn << 18) & 0x7f7f7f7f7f7f7f7f) == value && ((pawn << 9) & stones) != 0 { true }
-                else if ((pawn >> 18) & 0xfefefefefefefefe) == value && ((pawn >> 9) & stones) != 0 { true }
+                else if ((pawn << 2) & 0xfefefefefefefefe) == value && ((pawn << 1) & stones) != 0 { true }
+                else if ((pawn >> 2) & 0x7f7f7f7f7f7f7f7f) == value && ((pawn >> 1) & stones) != 0 { true }
+                else if ((pawn << 14) & 0xfefefefefefefefe) == value && ((pawn << 7) & stones) != 0 { true }
+                else if ((pawn >> 14) & 0x7f7f7f7f7f7f7f7f) == value && ((pawn >> 7) & stones) != 0 { true }
+                else if ((pawn << 18) & 0xfefefefefefefefe) == value && ((pawn << 9) & stones) != 0 { true }
+                else if ((pawn >> 18) & 0x7f7f7f7f7f7f7f7f) == value && ((pawn >> 9) & stones) != 0 { true }
                 else { false }
             },
             ChongAction { piece: ChongPiece::Stone, .. } => {
@@ -170,8 +170,8 @@ fn test_pawns(position: &ChongState) -> String {
         for c in 0..8 {
             let action = ChongAction { piece: ChongPiece::Pawn, r: r, c: c };
             output[r as usize][c as usize] = match position.is_legal(&action, &[]) {
-                true => 't' as u8,
-                false => 'f' as u8
+                true => '*' as u8,
+                false => '.' as u8
             }
         }
     }
@@ -183,22 +183,26 @@ fn test_pawns(position: &ChongState) -> String {
 
 
 fn main() {
-    let position = ChongState { pawn1: 0 * 8 + 3, pawn2: 0, stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
-    println!("{}", test_pawns(&position));
+    let position = ChongState { pawn1: 1 << (0 * 8 + 3), pawn2: 0,
+                                stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
+    println!("{}\n", test_pawns(&position));
 
-    let position = ChongState { pawn1: 3 * 8 + 5, pawn2: 0, stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
+    let position = ChongState { pawn1: 1 << (3 * 8 + 5), pawn2: 0,
+                                stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
+    println!("{}\n", test_pawns(&position));
 
+    let position = ChongState { pawn1: 1 << (7 * 8 + 4), pawn2: 0,
+                                stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
+    println!("{}\n", test_pawns(&position));
 
-    let position = ChongState { pawn1: 7 * 8 + 4, pawn2: 0, stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
+    let position = ChongState { pawn1: 1 << (5 * 8 + 0), pawn2: 0,
+                                stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
+    println!("{}\n", test_pawns(&position));
 
+    let position = ChongState { pawn1: 1 << (4 * 8 + 7), pawn2: 0,
+                                stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
+    println!("{}\n", test_pawns(&position));
 
-    let position = ChongState { pawn1: 5 * 8 + 0, pawn2: 0, stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
-
-
-    let position = ChongState { pawn1: 4 * 8 + 7, pawn2: 0, stones1: 0, stones2: 0, next: ChongPlayer::Player1 };
-
-
-    let action = ChongAction { piece: ChongPiece::Pawn, r: 1, c: 3 };
     // println!("{:?}", start.is_legal(&action, &[]));
     // println!("{:?}", start.next_state(&action));
 }
